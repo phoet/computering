fc27dc3a5e1c42e26e462c7960f37c566fed9c87
phoet <phoetmail@googlemail.com>
2017-02-02 10:51:34 +0100

    checkout some output stuffs

--- lib/computering/cmd.rb
+++ lib/computering/cmd.rb
@@ -32,15 +32,27 @@ module Computering
       end
     end
 
+    def write(item, index, actual, debug: false)
+      size = `tput cols`.chomp.to_i
+      line = "#{item.padding}#{item[0..index]}"
+      typed = debug ? "#{actual} <-" : ''
+      line  = line.ljust(size - typed.length) + typed
+      @stdout.print("#{line}\r")
+      @stdout.flush
+    end
+
     def readchars(item)
       i = 0
+      actual = ''
       @stdin.noecho do |io|
-        @stdout.write item.padding if item.padding
+        # write item, ""
         while char = io.getch
           raise Exit if char.ord == CONTROL_C
           raise Reload if char.ord == CONTROL_R
           raise Back if char.ord == CONTROL_B
+          actual += char
           if char.ord == ENTER
+            actual = ''
             @stdout.puts item[i..-1]
             item.execute
             @stdout.puts "#{item.padding}#{item.buffer}" if item.buffer
@@ -49,10 +61,10 @@ module Computering
           else
             if item[i]
               while item[i] =~ /\s/
-                @stdout.write item[i]
+                write item, i, actual
                 i += 1
               end
-              @stdout.write item[i]
+              write item, i, actual
             end
             i += 1
           end
@@ -61,3 +73,12 @@ module Computering
     end
   end
 end
+
+require 'time'
+#
+# loop do
+#   time = Time.now.to_s + "\r"
+#   print time
+#   $stdout.flush
+#   sleep 1
+# end
